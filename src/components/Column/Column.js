0bd5561e422e5bfe8e6f0efd602a2280daf3da29
Lenny <lenny.donnez@gmail.com>
2019-02-20 00:02:42 +0100

    Specify flex base based on resolution breakpoints
    Its now possible to add (xs, md, lg)={1 till 12} property to the column
    component to specify the breakpoints.

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,15 +1,32 @@
 import styled from "@emotion/styled"
 import theme from "../../theme"
 
+const flexBases = {
+  12: "100%",
+  11: "91,65%",
+  10: "83,32%",
+  9: "75%",
+  8: "66,66%",
+  7: "58,33%",
+  6: "50%",
+  5: "41,66%",
+  4: "33.33%",
+  3: "25%",
+  2: "16.66%",
+  1: "8.33%"
+}
+
 export default styled("div")(props => {
   return {
     padding: `${props.spacing}px`,
-    flexBasis: "100",
-    [theme.breakpoints.up("sm")]: {
-      flex: `${props.grow ? props.grow : "1"}`
+    [props.lg ? theme.breakpoints.up("lg") : null]: {
+       flexBasis: `${props.lg ? flexBases[props.lg] : "100%"}`
+    },
+    [props.md ? theme.breakpoints.down("md") : null]: {
+       flexBasis: `${props.md ? flexBases[props.md] : "100%"}`
     },
     [props.xs ? theme.breakpoints.down("sm") : null]: {
-      flex: `${props.grow ? props.grow : "1"}`
+       flexBasis: `${props.xs ? flexBases[props.xs] : "100%"}`
     }
   }
 })
--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -2,6 +2,6 @@ import styled from "@emotion/styled"
 
 export default styled.div`
   display: flex;
-  flex-direction: row;
   flex-wrap: wrap;
+  justify-content: flex-start;
 `
