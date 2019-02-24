9554d8f42ffcb1adcc05c3fbaf01c8fef07e412d
Lenny <lenny.donnez@gmail.com>
2019-02-24 14:31:01 +0100

    Add Loading Indicator

--- src/app/components/Lists/Lists.js
+++ src/app/components/Lists/Lists.js
@@ -3,63 +3,70 @@ import { connect } from "react-redux"
 import ListCard from "../ListCard"
 import Row from "components/Row"
 import Column from "components/Column"
+import LoadingIndicator from "components/LoadingIndicator"
 import { actions } from "../../redux/lists/lists.actions"
 import * as selectors from "../../redux/lists/lists.selectors"
 
 class Lists extends React.PureComponent {
-  state = {};
+  state = {}
 
   handlePanelChange = identifier => event => {
-    const expanded = this.state[identifier] ? false : true
-    this.setState({ [identifier]: expanded })
-  };
+  	const expanded = this.state[identifier] ? false : true
+  	this.setState({ [identifier]: expanded })
+  }
 
   componentDidMount = () => {
-    const { getLists } = this.props
-    getLists()
-  };
+  	const { getLists } = this.props
+  	getLists()
+  }
 
   render () {
-    const { lists } = this.props
-
-    return (
-      <React.Fragment>
-        <Row>
-          {lists &&
+  	const { lists, loading } = this.props
+  	return (
+  		<React.Fragment>
+  			<Row>
+  				{lists &&
             lists.map(l => {
-              return (
-                <Column key={l.id} spacing={8} lg={3} md={6} xs={12}>
-                  <ListCard
-                    list={l}
-                    onChange={this.handlePanelChange}
-                    expanded={this.state[l.id] && this.state[l.id]}
-                  />
-                </Column>
-              )
+            	return (
+            		<Column key={l.id} spacing={8} lg={3} md={6} xs={12}>
+            			<ListCard
+            				list={l}
+            				onChange={this.handlePanelChange}
+            				expanded={this.state[l.id] && this.state[l.id]}
+            			/>
+            		</Column>
+            	)
             })}
-        </Row>
-      </React.Fragment>
-    )
+  			</Row>
+  			{loading && (
+  				<Row>
+  					<Column lg={12} md={12} xs={12}>
+  						<LoadingIndicator />
+  					</Column>
+  				</Row>
+  			)}
+  		</React.Fragment>
+  	)
   }
 }
 
 const mapDispatchToProps = dispatch => {
-  return {
-    getLists: fields => {
-      dispatch(actions.get())
-    }
-  }
+	return {
+		getLists: fields => {
+			dispatch(actions.get())
+		}
+	}
 }
 
 const mapStateToProps = state => {
-  return {
-    errors: selectors.getErrors(state),
-    loading: selectors.isLoading(state),
-    lists: selectors.getLists(state)
-  }
+	return {
+		errors: selectors.getErrors(state),
+		loading: selectors.isLoading(state),
+		lists: selectors.getLists(state)
+	}
 }
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+	mapStateToProps,
+	mapDispatchToProps
 )(Lists)
