479277db41ee78f7c848949e495c320015494c0b
Lenny <lenny.donnez@gmail.com>
2019-02-24 19:26:58 +0100

    Make description fixed with for consitent design

---
+++ src/app/components/ListCard/components/Description.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { StyledTypography } from "./style"
+
+function Description ({ children }) {
+	return (
+		<StyledTypography paragraph >
+			{children}
+		</StyledTypography>
+	)
+}
+
+export default Description
---
+++ src/app/components/ListCard/components/style.js
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled"
+import Typography from "@material-ui/core/Typography"
+
+export const StyledTypography = styled(Typography)`
+		height: 20px
+`
