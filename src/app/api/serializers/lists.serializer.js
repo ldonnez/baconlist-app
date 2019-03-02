c6ddcf8bb9a0e32ae618ef45d1d737bd1b1dd825
Lenny <lenny.donnez@gmail.com>
2019-03-02 19:36:32 +0100

    Move toISODate to utils

--- src/app/api/serializers/lists.serializer.js
+++ src/app/api/serializers/lists.serializer.js
@@ -1,14 +1,12 @@
+import { dateToISO } from "utils"
+
 export const serializedList = (data) => {
 	return {
 		...data,
-		due_to: dateToISO(data.due_to)
+		due_to: dateToISO(data.due_to),
 	}
 }
 
-const dateToISO = (dateString) => {
-	if (dateString) {
-		const d = new Date(dateString)
-		return d.toISOString()
 	}
 }
 
