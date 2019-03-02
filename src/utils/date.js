11db34aac8063b5d428c60e9b9992562847399ce
Lenny <lenny.donnez@gmail.com>
2019-03-02 19:36:32 +0100

    Move toISODate to utils

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,6 +1,10 @@
 export const normalizedDate = date => {
 	if (date && isValidDate(date)) {
 		return new Date(date).toDateString()
+export const dateToISO = (dateString) => {
+	if (dateString) {
+		const d = new Date(dateString)
+		return d.toISOString()
 	}
 }
 
