3c4520865936203c383eda5659a0be3902fb5f9b
Lenny <lenny.donnez@gmail.com>
2019-03-08 18:17:11 +0100

    Remove not used selectors

--- src/app/redux/lists/lists.selectors.js
+++ src/app/redux/lists/lists.selectors.js
@@ -6,25 +6,3 @@ export const getLists = state => {
 export const getErrors = state => state.lists.errors
 export const isLoading = state => state.lists.isLoading
 export const getEditId = state => state.lists.editId
-
-export const getListById = (state, id) => {
-  if(getLists(state)) {
-    const listById = getLists(state).filter(l => l.id === id)
-    return listById && listById[0]
-  }
-}
-
-export const getTasksFromList = (state, id) => {
-  return getListById(id) && getListById(id).tasks
-}
-
-
-export const getTodoTasksFromList = (state, id) => {
-  const list = getListById(state, id)
-  return list && list.tasks && list.tasks.filter(t => t.completed === "false")
-}
-
-export const getCompletedTasksFromList = (state, id) => {
-  const list = getListById(state, id)
-  return list && list.tasks && list.tasks.filter(t => t.completed === "true")
-}
