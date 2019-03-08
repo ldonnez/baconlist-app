34507e62c519e4c65bd16826e75c185f2c08262c
Lenny <lenny.donnez@gmail.com>
2019-03-08 22:06:11 +0100

    When successfully sign up show dialog

--- src/app/redux/users/users.selectors.js
+++ src/app/redux/users/users.selectors.js
@@ -1,3 +1,5 @@
 export const getErrors = state => state.users.errors
 export const isLoading = state => state.users.isLoading
+export const isSuccess = state => state.users.success
 export const getUser = state => state.users.data
+
